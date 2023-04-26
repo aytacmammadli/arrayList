3a4bafb090038e35c06f8fd76b27c010a11847f7
Aytac <aytacmammadli6@gmail.com>
2023-04-26 21:06:40 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class Main {
+    public static void main(String[] args) {
+        LinkedList<Integer> arr=new LinkedList<>();
+       arr.add(0,56);
+       arr.add(5,78);
+
+
+
+     //   System.out.println(arr);
+    }
+}
